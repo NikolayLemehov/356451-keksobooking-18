648167a09fe91f3b64ebb0be631205fac81dc583
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-09-19 02:05:22 +0300

    Отображает ближайшую рекламу и исправляет ошибку со шрифтами.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var AD_QUANTITY = 8;
 var AVATAR_NUMBER = 8;
 var TITLE = 'Заголовок предложения';
 var MIN_LOCATION_Y = 130;
@@ -39,8 +40,17 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var pinsElement = map.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
 var randomInteger = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
+};
+var roundingRandomInteger = function (min, max, rounding) {
+  return Math.floor(randomInteger(min, max) / rounding) * rounding;
 };
 var shuffle = function (array) {
   for (var i = array.length - 1; i > 0; i--) {
@@ -54,8 +64,57 @@ var shuffle = function (array) {
 var createArraySerialInteger = function (number) {
   var array = [];
   for (var i = 0; i < number; i++) {
-    array.push(i + 1);
+    if (i < 9) {
+      array.push('img/avatars/user0' + (i + 1) + '.png');
+    }
+    if (i >= 9) {
+      array.push('img/avatars/user' + (i + 1) + '.png');
+    }
   }
   return array;
 };
-console.log(shuffle(createArraySerialInteger(AVATAR_NUMBER)));
+var createDataAd = function (avatarNumber, title, locationBlock, minLocationY, maxLocationY, minPrice, maxPrice, types, maxRooms, maxGuests, checkins, checkouts, features, description, photos, adQuantity) {
+  var array = [];
+  var avatars = shuffle(createArraySerialInteger(avatarNumber));
+  for (var i = 0; i < adQuantity; i++) {
+    var ad = {
+      'author': {
+        'avatar': avatars[i]
+      },
+      'offer': {
+        'title': title,
+        'address': randomInteger(1, 8) + ', ' + randomInteger(minLocationY, maxLocationY),
+        'price': roundingRandomInteger(minPrice, maxPrice, 1000),
+        'type': types[randomInteger(0, types.length - 1)],
+        'rooms': randomInteger(1, maxRooms),
+        'guests': randomInteger(1, maxGuests),
+        'checkin': checkins[randomInteger(0, checkins.length - 1)],
+        'checkout': checkouts[randomInteger(0, checkouts.length - 1)],
+        'features': shuffle(features).slice(randomInteger(0, features.length - 1)),
+        'description': description,
+        'photos': shuffle(photos).slice(randomInteger(0, photos.length - 1))
+      },
+      'location': {
+        'x': randomInteger(1, map.offsetWidth),
+        'y': randomInteger(minLocationY, maxLocationY)
+      }
+    };
+    array.push(ad);
+  }
+  return array;
+};
+var dataAds = createDataAd(AVATAR_NUMBER, TITLE, map, MIN_LOCATION_Y, MAX_LOCATION_Y, MIN_PRICE, MAX_PRICE, TYPES, MAX_ROOMS, MAX_GUESTS, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTION, PHOTOS, AD_QUANTITY);
+var renderAd = function (ad) {
+  var adElement = pinTemplate.cloneNode(true);
+  adElement.style.left = '' + (ad.location.x - adElement.offsetWidth / 2) + 'px';
+  adElement.style.top = '' + (ad.location.y - adElement.offsetHeight) + 'px';
+  adElement.querySelector('img').src = ad.author.avatar;
+  adElement.querySelector('img').alt = ad.offer.title;
+  // console.log(adElement.offsetWidth, adElement.offsetHeight);
+  return adElement;
+};
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < dataAds.length; i++) {
+  fragment.appendChild(renderAd(dataAds[i]));
+}
+pinsElement.appendChild(fragment);
