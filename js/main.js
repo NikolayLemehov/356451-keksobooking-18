1583375f4f78e1c28e6ec17e566e01a1d03abdb1
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-09-19 19:41:52 +0300

    Создаёт карточку объявления.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ var AVATAR_NUMBER = 8;
 var TITLE = 'Заголовок предложения';
 var MIN_LOCATION_Y = 130;
 var MAX_LOCATION_Y = 630;
-var MIN_PRICE = 20000;
-var MAX_PRICE = 200000;
-var TYPES = [
-  'palace',
-  'flat',
-  'house',
-  'bungalo',
-];
+var MIN_PRICE = 2000;
+var MAX_PRICE = 50000;
+var TYPES_TRANSLATION = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало',
+};
 var MAX_ROOMS = 4;
 var MAX_GUESTS = 4;
 var CHECKINS = [
@@ -31,21 +31,23 @@ var FEATURES = [
   'parking',
   'washer',
   'elevator',
-  'conditioner'
+  'conditioner',
 ];
 var DESCRIPTION = 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.';
 var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 var pinsElement = map.querySelector('.map__pins');
-var pinTemplate = document.querySelector('#pin')
-  .content
+var pinTemplate = document.querySelector('#pin').content
   .querySelector('.map__pin');
+var filterContainerElement = map.querySelector('.map__filters-container');
+var cardTemplate = document.querySelector('#card').content
+  .querySelector('.map__card');
 var randomInteger = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
@@ -73,18 +75,19 @@ var createArraySerialInteger = function (number) {
   }
   return array;
 };
-var createDataAd = function (avatarNumber, title, locationBlock, minLocationY, maxLocationY, minPrice, maxPrice, types, maxRooms, maxGuests, checkins, checkouts, features, description, photos, adQuantity) {
+var createDataAd = function (avatarNumber, title, locationBlock, minLocationY, maxLocationY, minPrice, maxPrice, typesTranslation, maxRooms, maxGuests, checkins, checkouts, features, description, photos, adQuantity) {
   var array = [];
   var avatars = shuffle(createArraySerialInteger(avatarNumber));
+  var types = Object.keys(typesTranslation);
   for (var i = 0; i < adQuantity; i++) {
     var ad = {
       'author': {
-        'avatar': avatars[i]
+        'avatar': avatars[i],
       },
       'offer': {
         'title': title,
         'address': randomInteger(1, 8) + ', ' + randomInteger(minLocationY, maxLocationY),
-        'price': roundingRandomInteger(minPrice, maxPrice, 1000),
+        'price': roundingRandomInteger(minPrice, maxPrice, 100),
         'type': types[randomInteger(0, types.length - 1)],
         'rooms': randomInteger(1, maxRooms),
         'guests': randomInteger(1, maxGuests),
@@ -92,19 +95,19 @@ var createDataAd = function (avatarNumber, title, locationBlock, minLocationY, m
         'checkout': checkouts[randomInteger(0, checkouts.length - 1)],
         'features': shuffle(features).slice(randomInteger(0, features.length - 1)),
         'description': description,
-        'photos': shuffle(photos).slice(randomInteger(0, photos.length - 1))
+        'photos': shuffle(photos).slice(randomInteger(0, photos.length - 1)),
       },
       'location': {
         'x': randomInteger(1, map.offsetWidth),
-        'y': randomInteger(minLocationY, maxLocationY)
-      }
+        'y': randomInteger(minLocationY, maxLocationY),
+      },
     };
     array.push(ad);
   }
   return array;
 };
-var dataAds = createDataAd(AVATAR_NUMBER, TITLE, map, MIN_LOCATION_Y, MAX_LOCATION_Y, MIN_PRICE, MAX_PRICE, TYPES, MAX_ROOMS, MAX_GUESTS, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTION, PHOTOS, AD_QUANTITY);
-var renderAd = function (ad) {
+var dataAds = createDataAd(AVATAR_NUMBER, TITLE, map, MIN_LOCATION_Y, MAX_LOCATION_Y, MIN_PRICE, MAX_PRICE, TYPES_TRANSLATION, MAX_ROOMS, MAX_GUESTS, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTION, PHOTOS, AD_QUANTITY);
+var renderPin = function (ad) {
   var adElement = pinTemplate.cloneNode(true);
   adElement.style.left = ad.location.x + 'px';
   adElement.style.top = ad.location.y + 'px';
@@ -115,6 +118,48 @@ var renderAd = function (ad) {
 };
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < dataAds.length; i++) {
-  fragment.appendChild(renderAd(dataAds[i]));
+  fragment.appendChild(renderPin(dataAds[i]));
 }
 pinsElement.appendChild(fragment);
+var isArrayInString = function (string, array) {
+  for (var j = 0; j < array.length; j++) {
+    if (string.includes(array[j])) {
+      return true;
+    }
+  }
+  return false;
+};
+var hideBlockInCollection = function (collection, arrayString) {
+  for (var j = 0; j < collection.length; j++) {
+    if (!isArrayInString(collection[j].getAttribute('class'), arrayString)) {
+      collection[j].style.display = 'none';
+    }
+  }
+};
+var renderPhotos = function (photoList, photoItem, photos) {
+  photoItem.src = photos[0];
+  if (photos.length > 1) {
+    var fragmentPhotoList = document.createDocumentFragment();
+    for (var j = 1; j < photos.length; j++) {
+      var photoElement = photoItem.cloneNode(true);
+      photoElement.src = photos[j];
+      fragmentPhotoList.appendChild(photoElement);
+    }
+    photoList.appendChild(fragmentPhotoList);
+  }
+};
+var renderCard = function (ad) {
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = ad.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = ad.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = TYPES_TRANSLATION[ad.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  hideBlockInCollection(cardElement.querySelectorAll('.popup__feature'), ad.offer.features);
+  cardElement.querySelector('.popup__description').textContent = ad.offer.description;
+  renderPhotos(cardElement.querySelector('.popup__photos'), cardElement.querySelector('.popup__photo'), ad.offer.photos);
+  cardElement.querySelector('.popup__avatar').src = ad.author.avatar;
+  return cardElement;
+};
+filterContainerElement.insertAdjacentElement('beforebegin', renderCard(dataAds[0]));
