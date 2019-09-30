56caec5d714e8d6c4cd41a0527f8d0a34dd09335
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-09-30 18:51:42 +0300

    Добавляет заголовку required. Накладывает ограничения на price.

--- js/init.js
+++ js/init.js
@@ -5,4 +5,5 @@
   window.form.init();
   window.form.adFormAddressInput.value = window.map.getCoordsElementOnMap(window.pin.mapPinMainBtn).centerX + ', ' +
     window.map.getCoordsElementOnMap(window.pin.mapPinMainBtn).centerY;
+  document.querySelector('#title').removeAttribute('required');
 })();
