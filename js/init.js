364fb495db43473767da486ebabf1df909bc029a
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-09-30 18:57:20 +0300

    Удаляет снятие required с title.

--- js/init.js
+++ js/init.js
@@ -5,5 +5,4 @@
   window.form.init();
   window.form.adFormAddressInput.value = window.map.getCoordsElementOnMap(window.pin.mapPinMainBtn).centerX + ', ' +
     window.map.getCoordsElementOnMap(window.pin.mapPinMainBtn).centerY;
-  document.querySelector('#title').removeAttribute('required');
 })();
