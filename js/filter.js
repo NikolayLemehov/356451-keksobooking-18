db17c102f6900d28424f22f483d3945198c2ad8f
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-13 02:40:06 +0300

    Добавляет фильлтрацию по типу, комнатам и гостям.

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+  var doPerChangeFilterAd = function () {
+    if (window.card.isShowCard) {
+      window.card.hidePinCard(window.card.indexShowCard);
+    }
+    window.filter.updateAds();
+  };
+
   var housingTypeSelect = document.querySelector('#housing-type');
   var type = housingTypeSelect.options[housingTypeSelect.selectedIndex].value;
   housingTypeSelect.addEventListener('change', function () {
@@ -8,18 +15,37 @@
   });
   var onTypeChange = function (filterType) {
     type = filterType;
-    window.card.hidePinCard(window.card.indexShowCard);
-    window.filter.updateAds();
+    doPerChangeFilterAd();
   };
+
+  var housingRoomsSelect = document.querySelector('#housing-rooms');
+  var roomNumber = housingRoomsSelect.options[housingRoomsSelect.selectedIndex].value;
+  housingRoomsSelect.addEventListener('change', function () {
+    onRoomChange(housingRoomsSelect.options[housingRoomsSelect.selectedIndex].value);
+  });
+  var onRoomChange = function (filterRoom) {
+    roomNumber = filterRoom;
+    doPerChangeFilterAd();
+  };
+
+  var housingGuestsSelect = document.querySelector('#housing-guests');
+  var guestNumber = housingGuestsSelect.options[housingGuestsSelect.selectedIndex].value;
+  housingGuestsSelect.addEventListener('change', function () {
+    onGuestChange(housingGuestsSelect.options[housingGuestsSelect.selectedIndex].value);
+  });
+  var onGuestChange = function (filterGuest) {
+    guestNumber = filterGuest;
+    doPerChangeFilterAd();
+  };
+
   window.filter = {
     updateAds: function () {
       var filteredAds = window.page.data.slice();
       filteredAds = filteredAds.filter(function (it) {
-        if (type === 'any') {
-          return true;
-        }
-        return it.offer.type === type;
-
+        var isTypeMatch = type === it.offer.type || type === 'any';
+        var isRoomMatch = roomNumber === it.offer.rooms.toString() || roomNumber === 'any';
+        var isGuestMatch = guestNumber === it.offer.guests.toString() || guestNumber === 'any';
+        return isTypeMatch && isRoomMatch && isGuestMatch;
       });
       window.pin.addPinsElement(filteredAds);
     },
